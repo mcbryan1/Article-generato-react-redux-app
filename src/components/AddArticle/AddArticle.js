dddb230abcb9a93822753b2bb0d5a906cce592eb
McBryan <solomonmcbryan@gmail.com>
2020-01-31 07:29:06 -0800

    Aricles.css created

--- src/components/AddArticle/AddArticle.js
+++ src/components/AddArticle/AddArticle.js
@@ -1,35 +1,41 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import "../Article/Article.css";
 
 const AddArticle = ({ saveArticle }) => {
-  const [article, setArticle] = useState()
+  const [article, setArticle] = useState();
 
   const handleArticleData = e => {
     setArticle({
       ...article,
-      [e.target.id]: e.target.value,
-    })
-  }
+      [e.target.id]: e.target.value
+    });
+  };
   const addNewArticle = e => {
-    e.preventDefault()
-    saveArticle(article)
-  }
+    e.preventDefault();
+    saveArticle(article);
+  };
 
   return (
-    <form onSubmit={addNewArticle} className="add-article">
-      <input
-        type="text"
-        id="title"
-        placeholder="Title"
-        onChange={handleArticleData}
-      />
-      <input
-        type="text"
-        id="body"
-        placeholder="Body"
-        onChange={handleArticleData}
-      />
-      <button>Add article</button>
-    </form>
-  )
-}
-export default AddArticle
+    <div className="article-1" >
+      <h1><u>Article Creator</u></h1>
+      <form onSubmit={addNewArticle} className="add-article">
+        <input
+          type="text"
+          id="title"
+          placeholder="Title"
+          onChange={handleArticleData}
+        />{" "}
+        <br />
+        <input
+          type="text"
+          id="body"
+          placeholder="Body"
+          onChange={handleArticleData}
+        />{" "}
+        <br />
+        <button>Add article</button>
+      </form>
+    </div>
+  );
+};
+export default AddArticle;
--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,10 +1,12 @@
 import React from "react"
-
+import "./Article.css"
 
 const article = ({ article }) => (
   <div className="article">
     <h1>{article.title}</h1>
     <p>{article.body}</p>
+
+    <hr/>
   </div>
 )
 
