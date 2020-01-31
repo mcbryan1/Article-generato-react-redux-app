a5223862eda9907b4e88222e649ecc6c1b2bad1b
McBryan <solomonmcbryan@gmail.com>
2020-01-31 06:59:36 -0800

    created articles.js, actionCreator, reducer, actionTypes

--- src/containers/articles.js
+++ src/containers/articles.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react"
+import React from "react"
+import { connect } from "react-redux"
+
 import Article from "../components/Article/Article"
 import AddArticle from "../components/AddArticle/AddArticle"
+import { addArticle } from "../store/actionCreators"
 
-const Articles = () => {
-  const [articles, setArticles] = useState([
-    { id: 1, title: "post 1", body: "Quisque cursus, metus vitae pharetra" },
-    { id: 2, title: "post 2", body: "Quisque cursus, metus vitae pharetra" },
-  ])
-  const saveArticle = e => {
-    e.preventDefault()
-    // the logic will be updated later
+const Articles = ({ articles, saveArticle }) => (
+  <div>
+    <AddArticle saveArticle={saveArticle} />
+    {articles.map(article => (
+      <Article key={article.id} article={article} />
+    ))}
+  </div>
+)
+
+const mapStateToProps = state => {
+  return {
+    articles: state.articles,
   }
-
-  return (
-    <div>
-      <AddArticle saveArticle={saveArticle} />
-      {articles.map(article => (
-        <Article key={article.id} article={article} />
-      ))}
-    </div>
-  )
 }
 
-export default Articles
+const mapDispatchToProps = dispatch => {
+  return {
+    saveArticle: article => dispatch(addArticle(article)),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Articles)
